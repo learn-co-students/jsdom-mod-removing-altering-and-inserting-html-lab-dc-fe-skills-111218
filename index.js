32de8691a2d9e4a4fcdff6e495f3cddc34c923e7
Greyson Harris <gharris@esri.com>
2018-12-13 02:13:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* Enter the code to remove the main node element under this comment */
+const maindiv = document.getElementById("main");
+maindiv.remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+var newHeader = document.createElement ("h1");
+document.body.appendChild (newHeader);
+newHeader.setAttribute ("id", "victory");
+var text = 'Bob Shnoodles is the champion!';
+newHeader.innerText = text;
