313388eabc81c3b082d602a200b5cfb42af404d3
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-08 00:11:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* Enter the code to remove the main node element under this comment */
 
+main.remove();
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+var newHeader = document.createElement('H1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute ('id', 'victory');
+newHeader.innerHTML = "Andrew is the champion";
